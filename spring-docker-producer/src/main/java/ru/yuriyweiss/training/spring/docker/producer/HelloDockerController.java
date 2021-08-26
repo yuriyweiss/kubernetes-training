e6d5fcf2117590dbbfd48c8dd34417a1cd0e3608
Yuriy Weiss <gweiss@yandex.ru>
2021-08-26 17:50:04 +0300

    add spring-docker-producer to git

---
+++ spring-docker-producer/src/main/java/ru/yuriyweiss/training/spring/docker/producer/HelloDockerController.java
@@ -0,0 +1,33 @@
+package ru.yuriyweiss.training.spring.docker.producer;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+import static java.time.format.DateTimeFormatter.ISO_LOCAL_DATE_TIME;
+
+@Slf4j
+@RestController
+public class HelloDockerController {
+
+    private static final String TOPIC_NAME = "spring.docker.producer.out";
+
+    private final KafkaProducer kafkaProducer;
+
+    @Autowired
+    public HelloDockerController( KafkaProducer kafkaProducer ) {
+        this.kafkaProducer = kafkaProducer;
+    }
+
+    @GetMapping( "/hello/{name}" )
+    public String hello( @PathVariable String name ) {
+        log.info( "incoming get request: " + name );
+        String message = "request: " + name + " " + LocalDateTime.now().format( ISO_LOCAL_DATE_TIME );
+        kafkaProducer.send( TOPIC_NAME, message );
+        return "processed " + name;
+    }
+}
---
+++ spring-docker-producer/src/main/java/ru/yuriyweiss/training/spring/docker/producer/KafkaProducer.java
@@ -0,0 +1,21 @@
+package ru.yuriyweiss.training.spring.docker.producer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KafkaProducer {
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    @Autowired
+    public KafkaProducer( KafkaTemplate<String, String> kafkaTemplate ) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void send( String topic, String message ) {
+        kafkaTemplate.send( topic, message );
+        kafkaTemplate.flush();
+    }
+}
